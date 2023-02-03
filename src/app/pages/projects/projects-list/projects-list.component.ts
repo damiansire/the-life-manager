1d6c046f7e87586578a75632de7ce3a4f80ae242
Damian Sire <damiansire@hotmail.com>
2023-02-03 01:43:24 -0300

    style(component): removed unused comment

--- src/app/pages/projects/projects-list/projects-list.component.ts
+++ src/app/pages/projects/projects-list/projects-list.component.ts
@@ -16,23 +16,3 @@ export class ProjectsListComponent implements OnInit {
   }
 
 }
-
-/*
-export interface Project {
-  name: string,
-  projectStatus: ProjectStatus
-}
-
-export interface GeneralProjectStatistics {
-  backlog: number,
-  inProgress: number,
-  completed: number,
-  total: number,
-}
-
-export interface GetProjectsReponse extends GeneralProjectStatistics {
-  projects: Project[]
-}
-
-
-*/
