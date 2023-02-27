390adf9936b339c0dd59c70700f49aeed9d518c8
Damian Sire <damiansire@hotmail.com>
2023-02-27 14:06:06 -0300

    feat: add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
+  { path: '', redirectTo: '/projects', pathMatch: 'full' },
   {
-    path: '',
-    loadChildren: () =>
-      import('./pages/projects/projects.module').then((m) => m.ProjectsModule),
+    path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then((m) => m.ProjectsModule),
   },
+  { path: "dashboard", loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: "doubt-tracker", loadChildren: () => import('./pages/doubt-tracker/doubt-tracker.module').then(m => m.DoubtTrackerModule) },
+  { path: "finance-tracker", loadChildren: () => import('./pages/finance-tracker/finance-tracker.module').then(m => m.FinanceTrackerModule) },
+  { path: "goal-tracker", loadChildren: () => import('./pages/goal-tracker/goal-tracker.module').then(m => m.GoalTrackerModule) },
+  { path: "job-search", loadChildren: () => import('./pages/job-search/job-search.module').then(m => m.JobSearchModule) },
+  { path: "projects", loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule) },
+  { path: "technology-event-tracking", loadChildren: () => import('./pages/technology-event-tracking/technology-event-tracking.module').then(m => m.TechnologyEventTrackingModule) },
+  { path: "trip-planner", loadChildren: () => import('./pages/trip-planner/trip-planner.module').then(m => m.TripPlannerModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
+
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  items = [
+    { path: 'projects', visibleName: "projects", icon: "" },
+    { path: "dashboard", visibleName: "dashboard", icon: "" },
+    { path: "doubt-tracker", visibleName: "doubt tracker", icon: "" },
+    { path: "finance-tracker", visibleName: "finance tracker", icon: "" },
+    { path: "goal-tracker", visibleName: "goal tracker", icon: "" },
+    { path: "job-search", visibleName: "job search", icon: "" },
+    { path: "projects", visibleName: "projects", icon: "" },
+    { path: "technology-event-tracking", visibleName: "technology event tracking", icon: "" },
+    { path: "trip-planner", visibleName: "trip planner", icon: "" },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
